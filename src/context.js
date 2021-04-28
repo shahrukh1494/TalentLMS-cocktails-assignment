3ba0af146367279be02b1d4e26ef73a7d482d69c
unknown <shahrukh1494@gmail.com>
2021-04-28 18:28:49 +0530

    first commit

---
+++ src/context.js
@@ -0,0 +1,62 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [cocktails, setCockTails] = useState([]);
+
+  const fetchDrinks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+      );
+      const data = await response.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const {
+            idDrink,
+            strDrink,
+            strDrinkThumb,
+            strAlcoholic,
+            strGlass,
+          } = item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCockTails(newCocktails);
+      } else {
+        setCockTails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm, fetchDrinks]);
+
+  return (
+    <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
